6261a1238bfc0d3547e98d6acf374da78014d2f1
Ian Gallardo <gallard3@gmail.com>
2024-09-25 17:07:16 -0400

    adding argTypes

--- stories/Hero.jsx
+++ stories/Hero.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
-export const Hero = ({ size, headline, ...props }) => {
+export const Hero = ({ size, headline, label, ...props }) => {
     return (
         <section className={[`hero-${size}`]}>
             <div class="uk-container">
                 <div className={[`hero-${size}__quad`]}>
-                    <h1 className={[`hero-${size}__title`]}>{headline}</h1>
+                    <h1 className={[`hero-${size}__title`]}>{label} {headline}</h1>
                 </div>
             </div>
         </section>
@@ -15,6 +15,7 @@ export const Hero = ({ size, headline, ...props }) => {
 };
 
 Hero.PropTypes = {
+    label: PropTypes.string,
     headline: PropTypes.string,
     size: PropTypes.oneOf(['sm', 'md', 'lg' ]),
 };
@@ -22,5 +23,6 @@ Hero.PropTypes = {
 
 Hero.defaultProps = {
     headline: "Lorem Ipsum",
+    label: "Areas to Support",
     size: 'md',
   };
--- stories/Hero.stories.js
+++ stories/Hero.stories.js
@@ -4,18 +4,31 @@ export default {
   title: "Example/Hero",
   component: Hero,
   tags: ["autodocs"],
+  argTypes: {
+    size: {
+      control: "radio",
+      options: ["sm", "md"],
+      description: "How large should the hero be?",
+    },
+    headline: {
+      control: "string",
+      description: "Text inside hero space",
+    },
+  },
 };
 
 export const Default = {
   args: {
     size: "sm",
-    headline: "Areas to Support",
+    label: "Areas to Support",
+    headline: "Lorem Ipsum",
   },
 };
 
 export const Medium = {
   args: {
     size: "md",
-    headline: "Areas to Support",
+    label: "Areas to Support",
+    headline: "Lorem Ipsum",
   },
 };
